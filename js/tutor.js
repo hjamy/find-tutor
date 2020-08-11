590fd8cc7f47e57185b0da77f43ef31a460f4719
Acer <jamy.hasanath03@gmail.com>
2020-08-11 13:58:14 +0600

    tutors 4 section done

--- js/tutor.js
+++ js/tutor.js
@@ -26,25 +26,60 @@ invitationBtn.addEventListener("click", function () {
     invitationDetails.style.display = "none"
 })
 
+const employerBtn = document.getElementById("employer-button");
+employerBtn.addEventListener("click", function () {
+    const employer = document.getElementById("employer");
+    employer.style.display = "block"
+    const board = document.getElementById("board");
+    board.style.display = "none"
+    const invitation = document.getElementById("invitation");
+    invitation.style.display = "none"
+    const profile = document.getElementById("profile");
+    profile.style.display = "none"
+    const employerId = document.getElementById("employer-id")
+    employerId.style.display = "none"
+})
+
+const profileBtn = document.getElementById("profile-button");
+profileBtn.addEventListener("click", function () {
+    const profile = document.getElementById("profile");
+    profile.style.display = "block"
+    const board = document.getElementById("board");
+    board.style.display = "none"
+    const invitation = document.getElementById("invitation");
+    invitation.style.display = "none"
+    const employer = document.getElementById("employer");
+    employer.style.display = "none"
+})
+
 
 //job-details show item event handler
-
-var elements = document.getElementsByClassName("serial-no");
-var myFunction = function () {
+var jobs = document.getElementsByClassName("job-serial");
+var jobFunction = function () {
     const jobDetails = document.getElementById("job-details")
     jobDetails.style.display = "block"
 };
-for (var i = 0; i < elements.length; i++) {
-    elements[i].addEventListener('click', myFunction, false);
+for (var i = 0; i < jobs.length; i++) {
+    jobs[i].addEventListener('click', jobFunction, false);
 }
 
 
 //invitation-details show item event handler
-var elements = document.getElementsByClassName("serial-no");
-var myFunction = function () {
+var invitations = document.getElementsByClassName("invitation-serial");
+var invitationFunction = function () {
     const invitationDetails = document.getElementById("invitation-details")
     invitationDetails.style.display = "block"
 };
-for (var i = 0; i < elements.length; i++) {
-    elements[i].addEventListener('click', myFunction, false);
+for (var i = 0; i < invitations.length; i++) {
+    invitations[i].addEventListener('click', invitationFunction, false);
+}
+
+//employer-details show item event handler
+var employers = document.getElementsByClassName("employer-individual");
+var employerFunction = function () {
+    const employerId = document.getElementById("employer-id")
+    employerId.style.display = "block"
+};
+for (var i = 0; i < employers.length; i++) {
+    employers[i].addEventListener('click', employerFunction, false);
 }
